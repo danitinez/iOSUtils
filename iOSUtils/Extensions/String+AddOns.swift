bdbb331aa5b06bcce8bdb28313fc84b5be99d616
Daniel Martinez <danitinez@gmail.com>
2020-02-12 12:22:00 -0300

    addon to String

--- iOSUtils/Extensions/String+AddOns.swift
+++ iOSUtils/Extensions/String+AddOns.swift
@@ -10,11 +10,25 @@ import Foundation
 
 
 public extension String {
-    var urlEscaped: String {
-        return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
-    }
+  var urlEscaped: String {
+      return addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!
+  }
 
-    var utf8Encoded: Data {
-        return data(using: .utf8)!
-    }
+  var utf8Encoded: Data {
+      return data(using: .utf8)!
+  }
+  
+  var isPhoneNumber: Bool {
+      do {
+          let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+          let matches = detector.matches(in: self, options: [], range: NSMakeRange(0, self.count))
+          if let res = matches.first {
+              return res.resultType == .phoneNumber && res.range.location == 0 && res.range.length == self.count
+          } else {
+              return false
+          }
+      } catch {
+          return false
+      }
+  }
 }
