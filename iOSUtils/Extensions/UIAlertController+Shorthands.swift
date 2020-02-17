4b70f17497aab164361e9147d1c1922eeb3dcdf9
Daniel Martinez <danitinez@gmail.com>
2020-02-17 16:02:51 -0300

    sheet actions helper

--- iOSUtils/Extensions/UIAlertController+Shorthands.swift
+++ iOSUtils/Extensions/UIAlertController+Shorthands.swift
@@ -33,16 +33,44 @@ public extension UIAlertController {
   }
   
   static func showActionSheet(_ title: String, message: String, options:[String], selectionHandler: @escaping SelectionHandler){
+    let sheetOptions: [SheetOptions] = options.map{SheetOptions.default(message: $0)}
+    showActionSheet(title, message: message, options: sheetOptions, selectionHandler: selectionHandler)
+  }
+
+  static func showActionSheet(_ title: String, message: String, options:[SheetOptions], selectionHandler: @escaping SelectionHandler){
+
     let controller = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
     
     for (index, element) in options.enumerated() {
-      let action =  UIAlertAction(title: element, style: .default, handler: {_ in
+      let action =  UIAlertAction(title: element.message, style: element.style, handler: {_ in
         selectionHandler(index)
       })
       controller.addAction(action)
     }
     if let showOn = UIApplication.topViewController() {
       showOn.present(controller, animated: true, completion: nil)
+    }
+  }
+  
+ enum SheetOptions {
+    case `default`(message: String)
+    case cancel(message: String)
+    case destructive(message: String)
+    
+    var style: UIAlertAction.Style  {
+      switch self {
+      case .`default`: return .default
+      case .cancel: return .cancel
+      case .destructive: return .destructive
+      }
+    }
+    
+    var message: String  {
+      switch self {
+      case let .`default`(msg): return msg
+      case let .cancel(msg): return msg
+      case let .destructive(msg): return msg
+      }
     }
   }
   
